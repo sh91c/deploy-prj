0f1312f918a9b32158489c660f10b29069910052
Choi Sang Hoon <sh91cxp@gmail.com>
2021-07-21 18:11:24 +0900

    settings debug false 수정

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
