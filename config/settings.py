08d934d9b6c1a3791b1c62aa2f4f93be0b298bf9
Choi Sang Hoon <sh91cxp@gmail.com>
2021-07-22 11:50:11 +0900

    sqlite3

--- config/settings.py
+++ config/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': f"{BASE_DIR}/'db.sqlite3'",
+        'NAME': f"{BASE_DIR}/db.sqlite3",
     }
 }
 
