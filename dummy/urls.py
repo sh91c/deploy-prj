dfa19b2a2e6c992cf7d9e65e2d9807986cd49d5e
Choi Sang Hoon <sh91cxp@gmail.com>
2021-07-26 17:29:31 +0900

    sync to async 데코레이터 테스트

--- dummy/urls.py
+++ dummy/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('get/async-view', views.sync_to_async_view),
     path('get/sync-view', views.sync_view),
-    path('get/non-deco-async-view', views.non_deco_async_view),
+    path('get/async-view-2', views.non_deco_async_view),
 ]
--- dummy/views.py
+++ dummy/views.py
@@ -7,42 +7,42 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.utils.decorators import sync_and_async_middleware
 
-@sync_and_async_middleware
-def simple_middleware(get_response):
-    # One-time configuration and initialization goes here.
-    if asyncio.iscoroutinefunction(get_response):
-        async def middleware(request):
-            # Do something here!
-            response = await get_response(request)
-            return response
-
-    else:
-        def middleware(request):
-            # Do something here!
-            response = get_response(request)
-            return response
-
-    return middleware
-
-@sync_to_async(thread_sensitive=False)
-@api_view(['GET'])
-def sync_to_async_view(request):
-    time.sleep(1)
-    return Response(
-        data={
-            "TEST sync_to_async TEST"
-        }, status=status.HTTP_200_OK
-    )
-
-
-@api_view(['GET'])
-def sync_view(request):
-    time.sleep(1)
-    return Response(
-        data={
-            "TEST sync TEST"
-        }, status=status.HTTP_200_OK
-    )
+# @sync_and_async_middleware
+# def simple_middleware(get_response):
+#     # One-time configuration and initialization goes here.
+#     if asyncio.iscoroutinefunction(get_response):
+#         async def middleware(request):
+#             # Do something here!
+#             response = await get_response(request)
+#             return response
+#
+#     else:
+#         def middleware(request):
+#             # Do something here!
+#             response = get_response(request)
+#             return response
+#
+#     return middleware
+#
+# @sync_to_async(thread_sensitive=False)
+# @api_view(['GET'])
+# def sync_to_async_view(request):
+#     time.sleep(1)
+#     return Response(
+#         data={
+#             "TEST sync_to_async TEST"
+#         }, status=status.HTTP_200_OK
+#     )
+#
+#
+# @api_view(['GET'])
+# def sync_view(request):
+#     time.sleep(1)
+#     return Response(
+#         data={
+#             "TEST sync TEST"
+#         }, status=status.HTTP_200_OK
+#     )
 
 @sync_to_async(thread_sensitive=True)
 @api_view(['GET'])
