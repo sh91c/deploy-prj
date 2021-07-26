cda8f0cf0d9b5c4f51c81b18b406022819b13444
Choi Sang Hoon <sh91cxp@gmail.com>
2021-07-26 14:00:15 +0900

    config __init__ 수정

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,32 @@
-import uvicorn
+import logging
+import logging.config
 
-log_config = uvicorn.config.LOGGING_CONFIG
-log_config["formatters"]["access"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
-log_config["formatters"]["default"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
+LOG_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {"default": {"format": {
+        "event": "access_log", "ip": "%(h)s", "status": "%(s)s", "method": "%(m)s", "path": "%(U)s", "referer": "%(f)s",
+        "x_session_id": "%(x-session-id)s", "x_google_id": "%(x-google-id)s", "x_server_time": "%(x-server-time)s",
+        "agent": "%(a)s"
+    }}},
+    "handlers": {
+        "console": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "level": "INFO",
+        }
+    },
+    "root": {"handlers": ["console"], "level": "INFO"},
+    "loggers": {
+        "gunicorn": {"propagate": True},
+        "gunicorn.access": {"propagate": True},
+        "gunicorn.error": {"propagate": True},
+        "uvicorn": {"propagate": True},
+        "uvicorn.access": {"propagate": True},
+        "uvicorn.error": {"propagate": True},
+    },
+}
+
+logging.config.dictConfig(LOG_CONFIG)
+logger = logging.getLogger(__name__)
