c4453f5a025ac4eb52421d7f2f6af39667153345
Choi Sang Hoon <sh91cxp@gmail.com>
2021-07-26 13:55:05 +0900

    config __init__ 수정

--- config/__init__.py
+++ config/__init__.py
@@ -1,28 +1,5 @@
-import logging
-import logging.config
+import uvicorn
 
-LOG_CONFIG = {
-    "version": 1,
-    "disable_existing_loggers": True,
-    "formatters": {"default": {"format": "%(asctime)s [%(process)s] %(levelname)s: %(message)s"}},
-    "handlers": {
-        "console": {
-            "formatter": "default",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stdout",
-            "level": "INFO",
-        }
-    },
-    "root": {"handlers": ["console"], "level": "INFO"},
-    "loggers": {
-        "gunicorn": {"propagate": True},
-        "gunicorn.access": {"propagate": True},
-        "gunicorn.error": {"propagate": True},
-        "uvicorn": {"propagate": True},
-        "uvicorn.access": {"propagate": True},
-        "uvicorn.error": {"propagate": True},
-    },
-}
-
-logging.config.dictConfig(LOG_CONFIG)
-logger = logging.getLogger(__name__)
+log_config = uvicorn.config.LOGGING_CONFIG
+log_config["formatters"]["access"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
+log_config["formatters"]["default"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
