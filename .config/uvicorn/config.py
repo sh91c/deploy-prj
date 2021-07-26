df2a467cee5a5e54ca250dab51c2a08c3ce023a8
Choi Sang Hoon <sh91cxp@gmail.com>
2021-07-26 13:42:31 +0900

    uvicorn 서비스 수정

--- .config/uvicorn/config.py
+++ .config/uvicorn/config.py
@@ -14,7 +14,6 @@ umask = 0
 user = "deploy"
 group = "deploy"
 tmp_upload_dir = None
-logger_class = 'uvicorn.config.LOGGING_CONFIG.loggers'
 accesslog = "/var/log/uvicorn/access.log"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 errorlog = "/var/log/uvicorn/error.log"
