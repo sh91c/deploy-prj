76f7f914e4bba3bf64be8d30f4a9dc75695382d4
Choi Sang Hoon <sh91cxp@gmail.com>
2021-07-26 17:30:16 +0900

    sync to async 데코레이터 테스트

--- dummy/urls.py
+++ dummy/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get/async-view', views.sync_to_async_view),
-    path('get/sync-view', views.sync_view),
+    # path('get/async-view', views.sync_to_async_view),
+    # path('get/sync-view', views.sync_view),
     path('get/async-view-2', views.non_deco_async_view),
 ]
